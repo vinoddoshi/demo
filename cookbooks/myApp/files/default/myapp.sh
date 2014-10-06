abb91623202c3dd288972dfe3e14f76047ec95a6
pandurang <ambekar10@gmail.com>
2014-10-06 14:47:59 +0530

    updated

--- cookbooks/myApp/files/default/myapp.sh
+++ cookbooks/myApp/files/default/myapp.sh
@@ -5,7 +5,7 @@
 #sudo mv -R prime_factors_kata /var/lib/jenkins/workspace/primefactor/
 
 sudo bundle update sqlite3
-sudo mkdir tmp
+#sudo mkdir tmp
 sudo chmod 677 -R tmp
 sudo chmod 677 -R db
 rake db:migrate

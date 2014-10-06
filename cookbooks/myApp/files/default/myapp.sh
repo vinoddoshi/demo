3a4ff2f874f44a701bd9bbf2fb89ff3694140741
pandurang <ambekar10@gmail.com>
2014-10-06 13:58:35 +0530

    Updated

--- cookbooks/myApp/files/default/myapp.sh
+++ cookbooks/myApp/files/default/myapp.sh
@@ -1,5 +1,12 @@
 #sudo rm -rf /prime_factors_kata
 #git clone https://github.com/Pandurang1024/prime_factors_kata.git
 
-git clone https://github.com/Pandurang1024/prime_factors_kata.git
-sudo mv -R prime_factors_kata /var/lib/jenkins/workspace/primefactor/
+#git clone https://github.com/Pandurang1024/prime_factors_kata.git
+#sudo mv -R prime_factors_kata /var/lib/jenkins/workspace/primefactor/
+
+sudo bundle update sqlite3
+sudo mkdir tmp
+sudo chmod 677 -R tmp
+sudo chmod 677 -R db
+rake db:migrate
+#rails s

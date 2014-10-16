b3bca66ccc22a6d8dfdcebb8e437fc28f914e643
pandurang <ambekar10@gmail.com>
2014-10-16 15:29:31 +0530

    Done

--- watirUI/test.rb
+++ watirUI/test.rb
@@ -1,3 +1,10 @@
+
+require 'watir'
+gem 'test-unit'
+require 'test/unit'
+
+class Search < Test::Unit::TestCase
+
 def generate(n)
     return [] if n == 1
     factor = (2..n).find {|x| n % x == 0} 
@@ -22,33 +29,59 @@ str = str + strvalues
 return str
 end
 
-require 'rubygems'
-require 'watir'
-require 'watir-webdriver'
 
+def setup
+@b = Watir::Browser.new :ff
+@b.goto "http://192.168.33.10:3000/numbers/new"
+end
 
-browser = Watir::Browser.start "http://ec2-54-167-138-99.compute-1.amazonaws.com:3000/numbers/new"
-browser.text_field(:id => 'number_prime').set '6'
-num = browser.text_field(:id => 'number_prime').value.to_i
+def teardown
+
+end
+
+def test_Search 
+@b.text_field(:id => 'number_prime').set '6'
+num = @b.text_field(:id => 'number_prime').value.to_i
 puts num
 result = generate (num)
 puts result
-browser.button(:value,"Create Number").click
+@b.button(:value,"Create Number").click
 
 str1 = finalResult(result)
 puts "Formated" << "#{str1.to_s} "
-Watir::Wait.until { browser.text.include? 'Number was successfully created' }
+Watir::Wait.until { @b.text.include? 'Number was successfully created' }
 puts " Actual Result:"
-if browser.text.include? "Number was successfully created"
+if @b.text.include? "Number was successfully created"
   puts "  Test Passed."
 else    
   puts "  Test Failed! "
 end
 str2 = "" << "#{str1.to_s}"
 
-if browser.text.include?"#{str2.to_s}"
-  puts "  Test Passed."
+time1 = Time.new
+
+
+File.open('testResult.txt', 'w') do |f2|  
+      f2.puts "Start Date and Time : " + time1.inspect
+      f2.puts "Entered Prime Number :" + num.to_s
+      f2.puts "Expected " + "#{str1.to_s} "
+      
+
+
+if @b.text.include?"#{str2.to_s}"
+  f2.puts "Actual prime factors shows same on page"
+  assert_equal 1, 1
 else    
-  puts "  Test Failed! "
+  f2.puts "Actual prime factors shows different on page"
+  assert_equal 1, 2
 end
+
+end  #File end
+
+
+end
+
+end
+
+
 

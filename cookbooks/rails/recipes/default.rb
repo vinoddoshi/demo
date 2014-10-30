e822c3e58052517a33bdfc180c68bad0612d8fc7
pandurang <ambekar10@gmail.com>
2014-10-30 18:05:18 +0530

    Comments

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -37,8 +37,7 @@ directory "/var/chef/prime_factors_kata/tmp" do
  action :create
 end
 
-
-%w[ /var/chef/prime_factors_kata/db  /var/chef/prime_factors_kata/tmp /var/chef/prime_factors_kata/Gemfile.lock ].each do |path|
+%w[ /var/chef/prime_factors_kata/db  /var/chef/prime_factors_kata/tmp /var/chef/prime_factors_kata/log /var/chef/prime_factors_kata/Gemfile.lock ].each do |path|
   directory path do
     owner 'root'
     group 'root'

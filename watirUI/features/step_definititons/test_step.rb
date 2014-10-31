2e52c0b9e06f3f4dde23279f2b95712a62302385
pandurang <ambekar10@gmail.com>
2014-10-31 19:29:01 +0530

    Add Cases

---
+++ watirUI/features/step_definititons/test_step.rb
@@ -0,0 +1,22 @@
+require 'watir-webdriver'
+require 'colorize'
+
+browser = Watir::Browser.new
+
+Given(/^From prime factor number$/) do
+  #pending # express the regexp above with the code you wish you had
+  browser.goto "http://192.168.33.11:3000/numbers/new"
+end
+
+When(/^user enter number$/) do
+  #pending # express the regexp above with the code you wish you had#
+  #browser.text_field(:id => 'number_prime').set '6'
+  browser.button(:value,"Create Number").click
+
+end
+
+Then(/^should display prime factors$/) do
+  puts "Should Enter Prime Number " .red
+  #pending # express the regexp above with the code you wish you had
+  
+end
---
+++ watirUI/test.feature
@@ -0,0 +1,6 @@
+Feature: Verify prime numbers factors
+ In order to see on numbers page we have to enter prime number
+Scenario: Verify prime numbers factors.
+Given From prime factor number
+When user enter number
+Then should display prime factors
